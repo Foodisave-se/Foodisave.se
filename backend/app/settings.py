7eb79efe1c427f942302684c431271fd7186c642
Kendal-dot <kendal.bal@icloud.com>
2025-03-05 12:17:18 +0100

    Changed settings.py

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -4,7 +4,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DB_URL: str
     GEMINI_API_KEY: str
-    GOOGLE_APPLICATION_CREDENTIALS: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
